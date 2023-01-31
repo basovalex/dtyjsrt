9deef75a142351fef1b021876d12fe6243c97f37
basovalex <60308980+basovalex@users.noreply.github.com>
2023-01-31 23:16:05 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -31,15 +31,12 @@ def check_date():
     if now.weekday()+1 == 7:
         day = d + datetime.timedelta(days=1)
     den = day.day
-    if d.day < 10:
+    if den.day < 10:
         den = '0' + str(day.day)
     month = day.month
-    if d.month < 10:
+    if day.month < 10:
         month = '0' + str(day.month)
-    if (now + datetime.timedelta(days=1)).month == now.month + 1:
-        month = day.month
-        print(month)
-    print(month)
+
 
 
 
